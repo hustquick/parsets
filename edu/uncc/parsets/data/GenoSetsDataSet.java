e134c835542d3f6a052534cd14876125573ffbc3
aacain <aacain@localhost>
2009-08-31 21:45:15 -0400

    Updated GenoSetsDataSet.java

---
+++ edu/uncc/parsets/data/GenoSetsDataSet.java
@@ -0,0 +1,75 @@
+package edu.uncc.parsets.data;
+
+import java.util.Iterator;
+import java.util.List;
+
+/** 
+ * Dataset that is backed by GenoSets Database
+ * @author aacain
+ *
+ */
+public class GenoSetsDataSet extends DataSet{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getNumCategoricalDimensions() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumDimensions() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumNumericDimensions() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumRecords() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DimensionHandle[] getNumericDimensions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoryTree getTree(List<DimensionHandle> dimensions) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<DimensionHandle> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
