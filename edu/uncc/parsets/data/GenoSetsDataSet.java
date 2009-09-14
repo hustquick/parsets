f33be2f7c9f6de8d4bc77fb233c73c9a19b8cbac
aacain <aacain@localhost>
2009-09-14 03:01:27 -0400

    Changed Genosets classes.

--- edu/uncc/parsets/data/GenoSetsDataSet.java
+++ edu/uncc/parsets/data/GenoSetsDataSet.java
@@ -1,20 +1,30 @@
 package edu.uncc.parsets.data;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import org.hibernate.Criteria;
+import org.hibernate.StatelessSession;
+
+import genosetsdb.GenoSetsClassMap;
+import genosetsdb.GenoSetsSessionManager;
+import genosetsdb.TableDimension;
+import genosetsdb.entity.Feature;
 
 /** 
  * Dataset that is backed by GenoSets Database
  * @author aacain
  *
  */
-public abstract class GenoSetsDataSet extends DataSet{
+public class GenoSetsDataSet extends DataSet{
 	
 	protected ArrayList<DimensionHandle> dimHandles;
 	protected Class factClass;
-	protected Map classMap;
+	protected TableDimension rootTableDimension;
+	protected Map<TableDimension, List<TableDimension>> classMap;
 	protected int numRecords;
 	protected String section = "Genosets";
 	
@@ -22,18 +32,56 @@ public abstract class GenoSetsDataSet extends DataSet{
 	public GenoSetsDataSet(String dbname, Class factClass){
 		this.name = dbname;
 		this.factClass = factClass;
-		
+		this.classMap = GenoSetsClassMap.getTableMap(factClass);
+		this.rootTableDimension = GenoSetsClassMap.getRootTableDimension();
 		loadDimensions();
-		loadClassMap();
+	}
+	
+	protected void loadDimensions(){
+		dimHandles = new ArrayList<DimensionHandle>();
+		//Iterate classMap and add all dimensions
+		TableDimension tableDimension = new TableDimension(Feature.class, null, "f");
+		GenoSetsDimensionHandle dim = new GenoSetsDimensionHandle("Type", "featureType1", "featureType", tableDimension, DataType.categorical, 0);
+		dimHandles.add(dim);
 	}
 
 	public CategoryTree getTree(List<DimensionHandle> dimensions){
-		Map<Class, ArrayList<GenoSetsDimensionHandle>> tableDimMap;
+		//Put all dimensions in a list with their parent table this avoids multiple joins
+		Map<TableDimension, List<GenoSetsDimensionHandle>> handleParentMap = new HashMap<TableDimension, List<GenoSetsDimensionHandle>>();
+		for (Iterator it = dimensions.iterator(); it.hasNext();) {
+			GenoSetsDimensionHandle dimensionHandle = (GenoSetsDimensionHandle) it.next();
+			List<GenoSetsDimensionHandle> list = handleParentMap
+							.get(dimensionHandle.getParentTable());
+			if(list == null){
+				list = new ArrayList(10);
+				list.add(dimensionHandle);
+				handleParentMap.put(dimensionHandle.getParentTable(), list);
+			}else
+				list.add(dimensionHandle);			
+		}
+		
+		//Iterate classMap, lookup tabledimension, and add dimensionhandles to query
+		StatelessSession session = GenoSetsSessionManager.getStatelessSession();
+		Criteria crit = session.createCriteria(rootTableDimension.getClass(), rootTableDimension.getAlias());
+		recursiveCriteria(rootTableDimension, crit, handleParentMap);		
 		
 		return null;
 	}
-	public abstract void loadClassMap();
-	protected abstract void loadDimensions();
+	
+	private void recursiveCriteria(TableDimension tableDim, Criteria criteria, Map<TableDimension, List<GenoSetsDimensionHandle>> handleParentMap){
+		List<TableDimension> childList = classMap.get(tableDim);
+		if(childList != null){
+			for (Iterator it = childList.iterator(); it.hasNext();) {
+				TableDimension childDim = (TableDimension) it.next();		
+				List<GenoSetsDimensionHandle> selectedDims = handleParentMap.get(childDim);
+				if(selectedDims != null){ //then add to criteria
+					System.out.println("Adding tableDim");
+					criteria.createCriteria(tableDim.getAlias() + "." + childDim.getPropertyName(), childDim.getAlias());
+				}
+				recursiveCriteria(childDim, criteria, handleParentMap);
+			}
+		}
+	}
 	
 	@Override
 	public int getNumCategoricalDimensions() {
@@ -97,6 +145,12 @@ public abstract class GenoSetsDataSet extends DataSet{
 		if (dimHandles == null)
 			loadDimensions();
 		return dimHandles.iterator();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("okay");
+		GenoSetsDataSet dset = new GenoSetsDataSet("dbName", Feature.class);
+		dset.getTree(dset.dimHandles);
 	}
 	
 }
