251f45c1a16ea6d16699dc02f11379dc1456faf5
aacain <aacain@localhost>
2009-09-14 03:01:27 -0400

    Changed Genosets classes.

--- edu/uncc/parsets/data/GenoSetsDimensionHandle.java
+++ edu/uncc/parsets/data/GenoSetsDimensionHandle.java
@@ -4,48 +4,80 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.hibernate.Criteria;
 import org.hibernate.StatelessSession;
+import org.hibernate.criterion.Projections;
 import org.hibernate.transform.Transformers;
 
 import genosetsdb.GenoSetsSessionManager;
+import genosetsdb.TableDimension;
+import genosetsdb.entity.Feature;
 
 
 
 public class GenoSetsDimensionHandle extends DimensionHandle{
-	private Class parentSuperDimension;
-	private String alias;
-	private String selectStmt;
+	private TableDimension parentTable;
+	private String propertyName;
 	
-	public GenoSetsDimensionHandle(String name, String handle, DataType dataType, int dimNum) {
+	public GenoSetsDimensionHandle(String name, String handle, String propertyName, TableDimension parentTable, DataType dataType, int dimNum) {
+		//TODO: change DimensionHandle constructor doesn't need LocalDBDataSet
+		//the null value is for the LocalDBDataSet 
 		super(name, handle, dataType, dimNum, null);
+		this.propertyName = propertyName;
+		this.parentTable = parentTable;
 	}
 	
 	
-	private void loadCategories(){
+	protected void loadCategories(){
 		categories = new ArrayList<CategoryHandle>();
+		
 		StatelessSession session = GenoSetsSessionManager.getStatelessSession();
+		Criteria crit = session.createCriteria(parentTable.getEntityClass(),
+				parentTable.getAlias());
+		crit.setProjection(Projections.projectionList()
+				.add(Projections.count(propertyName), "count")
+				.add(Projections.groupProperty(propertyName), handle));
 		
-		if(selectStmt == null)
-			selectStmt = this.handle;
 		
-		String query = "Select " + selectStmt + " as " + alias + 
-			" from " + parentSuperDimension.getPropertyName() + 
-			" group by " + this.handle;
-    	List<Map> list = session.createQuery(query).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+		
+		
+    	crit.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+    	List<Map> list = crit.list();
     	for (Map map : list) {
-			Object value = map.get(this.alias);
-			//categories.add(new CategoryHandle(value, this.propertyName + " " value, ));
+    		//TODO: typecasting to string might break if integer is returned
+    		String categoryName = (String)map.get(handle);
+			Integer count = (Integer)map.get("count");
+			categories.add(new CategoryHandle(categoryName, categoryName, 0, this, count));		
 		}
 	}
 	
+	
+	
+	public TableDimension getParentTable() {
+		return parentTable;
+	}
+
+
+	public void setParentTable(TableDimension parentTable) {
+		this.parentTable = parentTable;
+	}
+
+
+	public String getPropertyName() {
+		return propertyName;
+	}
+
+
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+
+
 	@Override
 	public Iterator<CategoryHandle> iterator() {
 		if (categories == null)
 			loadCategories();
 		return categories.iterator();
 	}
-	
-	public static void main(String[] args){
-		
-	}
+
 }
